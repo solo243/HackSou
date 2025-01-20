bbaee9181d9912ed2b1dc63437891c1801485000
Solo243 <welcomesir48u@gmail.com>
2025-01-20 11:26:19 +0530

    init

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import heroImage from "../assets/bg_2.svg";
 import obj from "../assets/object.png";
-import Counter from "./Counter";
+import Counter from "./Test1";
+
 
 const HeroSection = () => {
   return (
